9f2455adf1b1cc5df0d72e641589448600c71c9a
Anna <filatovaa@yahoo.com>
2020-09-13 23:59:03 -0700

    css is linked to index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,37 @@
 class App extends React.Component{
 
+    state={
+        items:[]
+    }
     componentDidMount(){
         fetch('/tracker')
         .then(response=>response.json())
         .then(data=>
-            console.log(data)
+            this.setState({
+                items: data
+            })
             )
 
     }
     render(){
+        console.log(this.state)
         return(
-            <h1>Hello there</h1>
+            <div>
+                <h1>My To Do </h1>
+                {this.state.items.map(item=>{
+                    return(
+                        <div className='item'> 
+                            <h3>{item.description}</h3>
+                            <h4>Complete</h4>
+                            <h4>X</h4>
+                         </div>
+
+
+                    )
+                
+                })}
+                
+            </div>
         )
     }
 }
