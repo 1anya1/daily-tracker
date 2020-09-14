d1f239a76773ccfad6955d94a5cde2f817886b54
Anna <filatovaa@yahoo.com>
2020-09-13 23:41:03 -0700

    reat side of view is rendering

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+class App extends React.Component{
+    render(){
+        return(
+            <h1>Hello there</h1>
+        )
+    }
+}
+ReactDOM.render(<App />, document.querySelector('.container'));
