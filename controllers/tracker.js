bf9b2a92898dfdb38662c856751e94544a9d6cde
Anna <filatovaa@yahoo.com>
2020-09-13 23:07:44 -0700

    index route is working

--- controllers/tracker.js
+++ controllers/tracker.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router();
 const Items = require('../models/tracker.js')
 
+//Index Route
 router.get('/', (req,res)=>{
-    res.send('index');
+    Items.find({}, (err, foundItems)=>{
+        res.json(foundItems)
+    })
 })
+//Post Route
 router.post('/', (req,res)=>{
     Items.create(req.body, (err, createdItem)=>{
         res.json(createdItem)
