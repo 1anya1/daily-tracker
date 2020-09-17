4e861b92346e38a52cdf3b514dea3f861186acc9
Anna <filatovaa@yahoo.com>
2020-09-16 23:33:43 -0700

    delete route implemented Front end

--- public/js/app.js
+++ public/js/app.js
@@ -14,17 +14,26 @@ class App extends React.Component{
             )
 
     }
+    deleteItem = (id, index)=>{
+        fetch('tracker/' + id,{
+            method: 'DELETE'
+        }).then((data)=>{
+            this.setState({
+                items: [...this.state.items.slice(0, index), ...this.state.items.slice(index+1)]
+            })
+        })
+    }
     render(){
         console.log(this.state)
         return(
             <div>
                 <h1>My To Do </h1>
-                {this.state.items.map(item=>{
+                {this.state.items.map((item, index)=>{
                     return(
                         <div className='item'> 
                             <h4>{item.description}</h4>
                             <h4>Complete</h4>
-                            <h4>X</h4>
+                            <h4 onClick={()=> this.deleteItem(item._id, index)}>X</h4>
                          </div>
 
 
